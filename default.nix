8eaaec97f6cf8a83a322591dbba39b48ee2094b4
Boris Polyakov <thewaterbox@yandex.ru>
2025-05-11 15:51:06 +0300

    hijacker: explicit pinning

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
-{ pkgs ? import <nixpkgs> { } }:
-{
+{pkgs ? import <nixpkgs> {}}: {
   # for nix-update
-  jj-fzf = pkgs.callPackage ./packages/jj-fzf/default.nix { };
+  jj-fzf = pkgs.callPackage ./packages/jj-fzf/default.nix {};
+  hijacker = pkgs.callPackage ./packages/hijacker {}; # Adaptation of my bash script by my good friend (IN RUST!!!) :3
 }
